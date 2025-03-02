6b35262fd6c0902707684bfe83ec4680496b5b42
frtavonatti <frtavonatti@gmail.com>
2025-03-02 17:04:10 +0000

    Refactor job listing components: extract JobList component for better code organization and reuse in JobListPage and MyJobsList.

--- client/src/pages/MyJobsList.jsx
+++ client/src/pages/MyJobsList.jsx
@@ -1,8 +1,7 @@
-import { Link } from "react-router-dom";
-import { useJobContext } from "../context/JobContext";
 import { useState, useEffect } from "react";
+import { useJobContext } from "../context/JobContext";
 import jobService from "../services/jobs";
-import Card from "../components/job/Card";
+import JobList from "../components/job/JobList";
 
 const MyJobsList = () => {
   const [{ jobs }, dispatch] = useJobContext();
@@ -19,35 +18,15 @@ const MyJobsList = () => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredJobs = jobs.filter((job) =>
-    job.title.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
-
   return (
-    <div className="px-8">
-      <div className="mt-8 flex items-center justify-between">
-        <h2 className="text-4xl font-bold">My Jobs</h2>
-        <Link to="/new">
-          <button className="btn">Create a JobPost</button>
-        </Link>
-      </div>
-
-      <form>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearch}
-          placeholder="Search jobs..."
-          className="mt-6 w-full rounded-lg border p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </form>
-
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {filteredJobs.map((job) => (
-          <Card key={job.id} job={job} />
-        ))}
-      </div>
-    </div>
+    <>
+      <JobList
+        jobs={jobs}
+        pageTitle="My Jobs"
+        searchTerm={searchTerm}
+        handleSearch={handleSearch}
+      />
+    </>
   );
 };
 
